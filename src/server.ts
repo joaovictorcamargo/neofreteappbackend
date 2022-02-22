bc1881edbe90e352c915329621656e0c354cd503
joaovictor <jc7155004@gmail.com>
2022-02-22 10:01:28 -0300

    devdependences

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
 const { NextFunction, Request, Response } = require("express");
 const express = require("express");
 require("express-async-errors");
-/* const { routes } = require("./routes"); */
-const { cors } = require("cors");
+const { routes } = require("./routes");
+/* const { cors } = require("cors"); */
 
 const app = express();
 /* app.use(cors()); */
 
 app.use(express.json());
 
-/* app.use(routes); */
+app.use(routes);
 
 //MidleWare
-/* app.use(
+app.use(
   (
     err: Error,
     request: typeof Request,
@@ -30,6 +30,6 @@ app.use(express.json());
       message: "Internal server error",
     });
   }
-); */
+);
 
 app.listen(process.env.PORT || 3000, () => console.log("Server is runing 🚀"));
